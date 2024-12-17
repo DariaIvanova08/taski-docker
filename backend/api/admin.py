47aafcda27d0421e4b7742eecc3701ebfa4c32a0
Дарья Иванова <dasha-ivanova24@mail.ru>
2024-12-17 22:55:19 +0300

    Add workflow with PEP8 linting

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,9 +1,12 @@
+"""admin."""
 from django.contrib import admin
 
 from .models import Task
 
 
 class TaskAdmin(admin.ModelAdmin):
+    """taskadmin."""
+
     list_display = ('title', 'description', 'completed')
 
 
